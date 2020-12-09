5f9246ca6f6954dde48e646b22f3aa898090e4fb
sahadat_hossain <sahadat_hossain@appscode.com>
2020-12-09 16:22:19 +0600

    done chapter 12 and did unit testing

---
+++ golang_book/testing/main_test.go
@@ -0,0 +1,69 @@
+package main
+
+import "testing"
+
+func TestTotal(t *testing.T) {
+	type args struct {
+		args []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "first test",
+			args: args{
+				args: []int{1,2,3},
+			},
+			want: 6,
+		},
+		{
+			name: "second one",
+			args: args{
+				args: []int{1,2},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Total(tt.args.args...); got != tt.want {
+				t.Errorf("Total() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestXor(t *testing.T) {
+	type args struct {
+		args []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test-1",
+			args: args{
+				args: []int{1, 2, 3},
+			},
+			want: 0,
+		},
+		{
+			name: "",
+			args: args{
+				args: []int{1, 2},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Xor(tt.args.args...); got != tt.want {
+				t.Errorf("Xor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
