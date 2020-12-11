ea5ef786fdfefd6da2d4da16a18e56708aed2191
sahadat_hossain <sahadat_hossain@appscode.com>
2020-12-11 11:12:21 +0600

    added input_output

---
+++ Input_Output/main.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// how to take input that has spaces on that
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter Your full name : ")
+	myname, _ := reader.ReadString('\n')
+	fmt.Println(myname)
+
+	// how to take an string input and then covert it to numeric value
+	reader2 := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your age in string : ")
+	myrating, _ := reader2.ReadString('\n')
+	mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(myrating), 64)
+	fmt.Println(mynumRating + 10)
+}
--- freeCodeCamp_tuto/main.go
+++ freeCodeCamp_tuto/main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
-
 func main() {
-	i := 5
+	//i := 5
+	//
+	//fmt.Printf("%v %T\n", i, i)
 
-	fmt.Printf("%v %T\n", i, i)
+	//var n bool = true
+	//fmt.Printf("%v, %T\n", n, n)
 
+	//n := 1 == 1
+	//m := 1 == 2
+	//
+	//fmt.Printf("%v, %T\n", n, n)
+	//fmt.Printf("%v, %T\n", m, m)
 
+	//a := 10 // 1010
+	//b := 3  // 0011
+	//fmt.Println(a & b)
+	//fmt.Println(a | b)
+	//fmt.Println(a ^ b)
+	//fmt.Println(a &^ b)
+
+	//s := "hello"
+	//s[0] = 'e'
+	//
+	//fmt.Println(s)
 }
+
